040ecf2fc0f968eae71a8f41978f2e179bcb191e
jon-jh <hiebert456@gmail.com>
2024-10-29 15:54:40 -0600

    Add more tests for more coverage

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios"; // imported for test
 import {
   getByPlaceholderText,
   getByText,
@@ -7,10 +8,61 @@ import {
   getByAltText,
   render,
   queryByText,
+  queryByAltText,
 } from "@testing-library/react";
 import Application from "components/Application";
 import { fireEvent } from "@testing-library/react";
 import { prettyDOM } from "@testing-library/react";
+
+// axios mock test for displaying error messages, day 2
+describe("Testing the error messages", () => {
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+    const { container, debug } = render(<Application />);
+    await findByText(container, "Archie Cohen");
+    const appointment = getAllByTestId(container, "appointment")[0];
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+    // Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    // Wait for the error message to be displayed.
+    await findByText(appointment, "Could not book appointment.");
+    // Check that the error message is displayed.
+    expect(
+      getByText(appointment, "Could not book appointment.")
+    ).toBeInTheDocument();
+    debug();
+  });
+
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+    const { container, debug } = render(<Application />);
+    await findByText(container, "Archie Cohen");
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+    // Check that the confirmation message is shown.
+    expect(
+      getByText(appointment, "Are you sure you would like to delete?")
+    ).toBeInTheDocument();
+    // Click the "Confirm" button on the confirmation.
+    fireEvent.click(getByText(appointment, "Confirm"));
+    // Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    // Wait for the error message to be displayed.
+    await findByText(appointment, "Could not cancel appointment.");
+    // Check that the error message is displayed.
+    expect(
+      getByText(appointment, "Could not cancel appointment.")
+    ).toBeInTheDocument();
+    debug();
+  });
+});
 
 // Testing with the debug function, day 2
 describe("Continued testing with Debug instead of PrettyDom", () => {
@@ -26,13 +78,49 @@ describe("Continued testing with Debug instead of PrettyDom", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
 
-    console.log("Debugging after clicking Save:");
-    debug();
+    // console.log("Debugging after clicking Save:");
+    // debug();
 
     // Verify that the appointment element does not contain the text "Saving"
-    expect(queryByText(appointment, "Saving")).not.toBeInTheDocument();
+    expect(queryByText(appointment, "Saving")).toBeInTheDocument();
     // We have not implemented any status messages so it is not there...
+  });
 
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await findByText(container, "Archie Cohen");
+
+    // 3. Click the "Delete" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+
+    // 4. Check that the confirmation message is shown.
+    expect(
+      getByText(appointment, "Are you sure you would like to delete?")
+    ).toBeInTheDocument();
+
+    // 5. Click the "Confirm" button on the confirmation.
+    fireEvent.click(queryByText(appointment, "Confirm"));
+
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    // 7. Wait until the element with the "Add" button is displayed.
+    await queryByAltText(appointment, "Add");
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+    // debug();
   });
 });
 
